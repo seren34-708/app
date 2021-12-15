899cdd2235f11749db88e60125a451f0cc05fd56
Naoya <nanetto777@gmail.com>
2021-12-15 20:23:42 +0900

    Hello World First commit

---
+++ swift/SwiftUI2020/HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by matuda naoya on 2021/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
